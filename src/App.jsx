c83e5e202c571d7a4cb93c9381aaab7212847fd2
Lesin Fargan <lesinfn@gmail.com>
2026-03-21 19:59:41 +0530

    Fixes made to the web and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 
 // Global Components
 import Navbar from "./Components/Navbar";
@@ -12,6 +12,7 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 // Admin
 import { AdminAuthProvider, RequireAdmin } from "./admin/AdminAuthContext";
@@ -23,6 +24,59 @@ import AdminSettings from "./admin/AdminSettings";
 // Context
 import { PopupProvider } from "./context/PopupContext";
 
+function AppContent() {
+  const location = useLocation();
+  const publicPaths = ["/", "/about", "/projects", "/services", "/contact"];
+  const isAdminPath = location.pathname.startsWith("/admin");
+  const isKnownPublicPath = publicPaths.includes(location.pathname);
+  const showShell = isAdminPath || isKnownPublicPath;
+
+  return (
+    <>
+      {showShell && <Navbar />}
+
+      <Routes>
+        {/* Public */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Admin */}
+        <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route
+          path="/admin/projects"
+          element={
+            <RequireAdmin>
+              <AdminLayout>
+                <AdminProjects />
+              </AdminLayout>
+            </RequireAdmin>
+          }
+        />
+        <Route
+          path="/admin/settings"
+          element={
+            <RequireAdmin>
+              <AdminLayout>
+                <AdminSettings />
+              </AdminLayout>
+            </RequireAdmin>
+          }
+        />
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      {showShell && <Footer />}
+      <QuotePopup />
+    </>
+  );
+}
+
 //Functions
 function App() {
   return (
@@ -30,48 +84,7 @@ function App() {
       <AdminAuthProvider>
         <Router>
           <ScrollToTop />
-
-          {/* NAVBAR */}
-          <Navbar />
-
-          {/* ROUTES */}
-          <Routes>
-            {/* Public */}
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-
-            {/* Admin */}
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route
-              path="/admin/projects"
-              element={
-                <RequireAdmin>
-                  <AdminLayout>
-                    <AdminProjects />
-                  </AdminLayout>
-                </RequireAdmin>
-              }
-            />
-            <Route
-              path="/admin/settings"
-              element={
-                <RequireAdmin>
-                  <AdminLayout>
-                    <AdminSettings />
-                  </AdminLayout>
-                </RequireAdmin>
-              }
-            />
-          </Routes>
-
-          {/* FOOTER */}
-          <Footer />
-
-          {/* GLOBAL POPUP */}
-          <QuotePopup />
+          <AppContent />
         </Router>
       </AdminAuthProvider>
     </PopupProvider>
